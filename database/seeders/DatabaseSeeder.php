077e0d837d7acabbdddec8ad6864678382a5d402
VishwanathOnGit <putchavishwanath@gmail.com>
2024-03-22 20:55:56 +0530

    Add User seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 
 use App\Models\Book;
 use App\Models\Review;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,31 +16,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Book::factory(33)->create()->each(function($book) {
-            $numReviews = random_int(5, 30);
+        User::factory(30)->create();
+        // Book::factory(33)->create()->each(function($book) {
+        //     $numReviews = random_int(5, 30);
 
-            Review::factory()->count($numReviews)
-            ->good()
-            ->for($book)
-            ->create();
-        });
+        //     Review::factory()->count($numReviews)
+        //     ->good()
+        //     ->for($book)
+        //     ->create();
+        // });
 
-        Book::factory(33)->create()->each(function($book) {
-            $numReviews = random_int(5, 30);
+        // Book::factory(33)->create()->each(function($book) {
+        //     $numReviews = random_int(5, 30);
 
-            Review::factory()->count($numReviews)
-            ->average()
-            ->for($book)
-            ->create();
-        });
+        //     Review::factory()->count($numReviews)
+        //     ->average()
+        //     ->for($book)
+        //     ->create();
+        // });
 
-        Book::factory(34)->create()->each(function($book) {
-            $numReviews = random_int(5, 30);
+        // Book::factory(34)->create()->each(function($book) {
+        //     $numReviews = random_int(5, 30);
 
-            Review::factory()->count($numReviews)
-            ->bad()
-            ->for($book)
-            ->create();
-        });
+        //     Review::factory()->count($numReviews)
+        //     ->bad()
+        //     ->for($book)
+        //     ->create();
+        // });
     }
 }
