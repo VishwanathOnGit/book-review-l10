0128a58a9aed61ab7f1513d798b0f15ae009592c
VishwanathOnGit <putchavishwanath@gmail.com>
2024-03-22 20:56:08 +0530

    Added User Collection for API response

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\BookController;
-use App\Mail\OrderShipped;
+use App\Http\Resources\UserCollection;
 use App\Mail\Test;
+use App\Models\User;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return new Test();
     Mail::send(new Test());
     // Mail::send(new OrderShipped());
     return view('welcome');
+});
+
+Route::get('api-test', function () {
+    return new UserCollection(User::get());
 });
 
 Route::resource('books', BookController::class);
